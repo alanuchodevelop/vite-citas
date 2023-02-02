4066a75a4ba0d7fd2eae079f60f553563d596556
Jesus Alan Aguilar Vazquez <jesusalan007@nube.unadmexico.mx>
2023-02-01 20:10:03 -0600

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+
+// componente principal de la aplicación
+function App() {
+
+    return (
+        <div className="App">
+            <h1>Hola Mundo</h1>
+        </div>
+    )
+}
+
+export default App
